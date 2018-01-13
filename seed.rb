a2b2842251b47b22117568de103bc5accd760ad0
Max Cherry <mxmCherry@gmail.com>
2018-01-13 20:11:11 +0200

    Fix seed.rb quoting

--- seed.rb
+++ seed.rb
@@ -4,6 +4,6 @@ c = Disque.new ENV['DISQUE_ADDRS']
 
 5.times do |i|
   20.times do |j|
-    c.push "queue-#{i}", '{ "job": #{j} }', 10000
+    c.push "queue-#{i}", %Q'{ "job": #{j} }', 10000
   end
 end
